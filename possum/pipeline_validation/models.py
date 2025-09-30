2568e5606f26d5eec0a33e375a18b88c6e228639
riniangreani <rini.angreani@curtin.edu.au>
2025-09-30 10:21:00 +0800

    Add list for type

--- possum/pipeline_validation/models.py
+++ possum/pipeline_validation/models.py
@@ -12,17 +12,24 @@ PIPELINE_STATE = (
         ('Failed', 'Failed'),
         ('WaitingForValidation', 'WaitingForValidation'),)
 
+PARTIAL_TILES_TYPE = (
+        ('center', 'center'),
+        ('edge', 'edge'),
+        ('corner', 'corner'),
+        ('edge - crosses projection boundary!', 'edge - crosses projection boundary!'),
+        ('corner - crosses projection boundary!', 'corner - crosses projection boundary!'),)
+
 class PartialTilePipelineRegionsBand1(models.Model):
     id = models.BigAutoField(primary_key=True, db_column='id')
-    observation = models.ForeignKey('survey.Observation', models.DO_NOTHING, db_column='observation', blank=True, null=True)
-    sbid = models.BigIntegerField(blank=True, null=True, db_column='sbid')
+    observation = models.ForeignKey('survey.Observation', models.DO_NOTHING, db_column='observation')
+    sbid = models.BigIntegerField(db_column='sbid')
     tile1 = models.ForeignKey('survey.Tile', models.DO_NOTHING, db_column='tile1', to_field='tile', related_name='band1_tile1')
-    tile2 = models.ForeignKey('survey.Tile', models.DO_NOTHING, db_column='tile2', to_field='tile', related_name='band1_tile2')
-    tile3 = models.ForeignKey('survey.Tile', models.DO_NOTHING,db_column='tile3', to_field='tile', related_name='band1_tile3')
-    tile4 = models.ForeignKey('survey.Tile', models.DO_NOTHING,db_column='tile4', to_field='tile', related_name='band1_tile4')
-    type = models.CharField(blank=True, null=True, db_column='type')
+    tile2 = models.ForeignKey('survey.Tile', models.DO_NOTHING, blank=True, null=True, db_column='tile2', to_field='tile', related_name='band1_tile2')
+    tile3 = models.ForeignKey('survey.Tile', models.DO_NOTHING, blank=True, null=True, db_column='tile3', to_field='tile', related_name='band1_tile3')
+    tile4 = models.ForeignKey('survey.Tile', models.DO_NOTHING, blank=True, null=True, db_column='tile4', to_field='tile', related_name='band1_tile4')
+    type = models.CharField(choices=PARTIAL_TILES_TYPE, db_column='type')
     number_sources = models.PositiveIntegerField(blank=True, null=True, db_column='number_sources')
-    _1d_pipeline = models.CharField(choices=PIPELINE_STATE, db_column='1d_pipeline')
+    _1d_pipeline = models.CharField(choices=PIPELINE_STATE, db_column='1d_pipeline', blank=True, null=True)
 
     class Meta:
         managed = False
@@ -31,15 +38,15 @@ class PartialTilePipelineRegionsBand1(models.Model):
 
 class PartialTilePipelineRegionsBand2(models.Model):
     id = models.BigAutoField(primary_key=True, db_column='id')
-    observation = models.ForeignKey('survey.Observation', models.DO_NOTHING, db_column='observation', to_field='name', blank=True, null=True)
-    sbid = models.BigIntegerField(blank=True, null=True, db_column='sbid')
+    observation = models.ForeignKey('survey.Observation', models.DO_NOTHING, db_column='observation', to_field='name')
+    sbid = models.BigIntegerField(db_column='sbid')
     tile1 = models.ForeignKey('survey.Tile', models.DO_NOTHING, db_column='tile1', related_name='band2_tile1')
-    tile2 = models.ForeignKey('survey.Tile', models.DO_NOTHING, db_column='tile2', related_name='band2_tile2')
-    tile3 = models.ForeignKey('survey.Tile', models.DO_NOTHING,db_column='tile3', related_name='band2_tile3')
-    tile4 = models.ForeignKey('survey.Tile', models.DO_NOTHING,db_column='tile4', related_name='band2_tile4')
-    type = models.CharField(blank=True, null=True, db_column='type')
+    tile2 = models.ForeignKey('survey.Tile', models.DO_NOTHING, blank=True, null=True, db_column='tile2', related_name='band2_tile2')
+    tile3 = models.ForeignKey('survey.Tile', models.DO_NOTHING, blank=True, null=True, db_column='tile3', related_name='band2_tile3')
+    tile4 = models.ForeignKey('survey.Tile', models.DO_NOTHING, blank=True, null=True, db_column='tile4', related_name='band2_tile4')
+    type = models.CharField(choices=PARTIAL_TILES_TYPE, db_column='type')
     number_sources = models.PositiveIntegerField(blank=True, null=True, db_column='number_sources')
-    _1d_pipeline = models.CharField(choices=PIPELINE_STATE, db_column='1d_pipeline')
+    _1d_pipeline = models.CharField(choices=PIPELINE_STATE, db_column='1d_pipeline', blank=True, null=True)
 
     class Meta:
         managed = False
@@ -71,7 +78,7 @@ class ObservationBand2_1DPipelineValidation(models.Model):
 class SurveyTiles_3DPipelineBand1(models.Model):
     tile_id = models.OneToOneField('survey.Tile', models.DO_NOTHING, db_column='tile_id', to_field='tile', primary_key=True)
     _3d_pipeline = models.CharField(blank=True, null=True, db_column='3d_pipeline')
-    _3d_pipeline_val = models.CharField(choices=VALIDATED_STATE, db_column='3d_pipeline_val')
+    _3d_pipeline_val = models.CharField( blank=True, null=True, choices=VALIDATED_STATE, db_column='3d_pipeline_val')
     _3d_pipeline_ingest = models.CharField(blank=True, null=True, db_column='3d_pipeline_ingest')
     _3d_pipeline_validator = models.CharField(blank=True, null=True, db_column='3d_pipeline_validator')
     _3d_val_link = models.CharField(blank=True, null=True, db_column='3d_val_link')
@@ -83,7 +90,7 @@ class SurveyTiles_3DPipelineBand1(models.Model):
 class SurveyTiles_3DPipelineBand2(models.Model):
     tile_id = models.OneToOneField('survey.Tile', models.DO_NOTHING, db_column='tile_id', to_field='tile', primary_key=True)
     _3d_pipeline = models.CharField(blank=True, null=True, db_column='3d_pipeline')
-    _3d_pipeline_val = models.CharField(choices=VALIDATED_STATE, db_column='3d_pipeline_val')
+    _3d_pipeline_val = models.CharField( blank=True, null=True, choices=VALIDATED_STATE, db_column='3d_pipeline_val')
     _3d_pipeline_ingest = models.CharField(blank=True, null=True, db_column='3d_pipeline_ingest')
     _3d_pipeline_validator = models.CharField(blank=True, null=True, db_column='3d_pipeline_validator')
     _3d_val_link = models.CharField(blank=True, null=True, db_column='3d_val_link')
